a1cc285d6033e7a620496366eddb1444377c6fb2
rajibkhan976 <rajibkhan976@gmail.com>
2025-08-31 21:44:43 +0800

    lab 5 completed

--- lab-5.py
+++ lab-5.py
@@ -1,20 +1,26 @@
-# import random
+# # Name: Rajib Hossain Khan
+# # Student ID: 20145454
+# # Version: 202009081123
 
-# a = random.randint(1, 100)
-# b = random.randint(1, 100)
+# section 2 question 6
+import random
 
-# result = a + b
-# userInput = 0
+a = random.randint(1, 100)
+b = random.randint(1, 100)
 
-# while result != userInput:
-#     try:
-#         if result == userInput:
-#             break
-#         userInput = input(f"Please enter the result of {a} + {b} = ")
-#         userInput = int(userInput)
-#     except:
-#         print("Please enter a valid integer")
+result = a + b
+userInput = 0
 
+while result != userInput:
+    try:
+        if result == userInput:
+            break
+        userInput = input(f"Please enter the result of {a} + {b} = ")
+        userInput = int(userInput)
+    except:
+        print("Please enter a valid integer")
+
+# section 3 question 4
 
 oneWord = input("Please enter one word: ")
 anotherWord = input("Please enter another word: ")
@@ -36,3 +42,28 @@ elif countOfLetterInSecondWord > countOfLetterInFirstWord:
     print(f"{matchingLetter} has more mtaches in {anotherWord}")
 else:
     print(f"{matchingLetter} has same number of matches in {oneWord} & {anotherWord}")
+
+# section 4
+
+numCount = 0
+total = 0
+userNum = input(f"Enter 'done' or number {numCount}: ")
+
+# running a loop to check if user wants to finish the task
+while userNum != "done":
+    # checking if the user input is a number
+    if userNum.isnumeric():
+        try:
+            userNum = int(userNum)
+            total += userNum
+            numCount += 1
+        except ValueError:
+            print(ValueError)
+        userNum = input(f"Enter 'done' or number {numCount}: ")
+    else:
+        print("Your input was invalid, try again")
+        userNum = input(f"Enter 'done' or number {numCount}: ")
+
+# printing the output of the task
+print(f"You input a total of {numCount} numbers.")
+print(f"The sum of those numbers is {total}")
