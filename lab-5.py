9dd9239d198e0ebd852d98b3ae667124154803d2
rajibkhan976 <rajibkhan976@gmail.com>
2025-08-31 20:55:11 +0800

    section 3 problem solved

--- lab-5.py
+++ lab-5.py
@@ -1,25 +1,38 @@
-# # Name: Rajib Hossain Khan
-# # Student ID: 20145454
-# # Version: 202009081123
+# import random
 
-# x = 0
-# while True:
-#     print(x)
-#     x += 1
+# a = random.randint(1, 100)
+# b = random.randint(1, 100)
 
-import random
+# result = a + b
+# userInput = 0
 
-a = random.randint(1, 100)
-b = random.randint(1, 100)
+# while result != userInput:
+#     try:
+#         if result == userInput:
+#             break
+#         userInput = input(f"Please enter the result of {a} + {b} = ")
+#         userInput = int(userInput)
+#     except:
+#         print("Please enter a valid integer")
 
-result = a + b
-userInput = 0
 
-while result != userInput:
-    try:
-        if result == userInput:
-            break
-        userInput = input(f"Please enter the result of {a} + {b} = ")
-        userInput = int(userInput)
-    except:
-        print("Please enter a valid integer")
+oneWord = input("Please enter one word: ")
+anotherWord = input("Please enter another word: ")
+matchingLetter = input("Please enter a letter you want to match: ")
+countOfLetterInFirstWord = 0
+countOfLetterInSecondWord = 0
+
+for i in oneWord:
+    if matchingLetter == i:
+        countOfLetterInFirstWord += 1
+
+for c in anotherWord:
+    if matchingLetter == c:
+        countOfLetterInSecondWord += 1
+
+if countOfLetterInFirstWord > countOfLetterInSecondWord:
+    print(f"{matchingLetter} has more mtaches in {oneWord}")
+elif countOfLetterInSecondWord > countOfLetterInFirstWord:
+    print(f"{matchingLetter} has more mtaches in {anotherWord}")
+else:
+    print(f"{matchingLetter} has same number of matches in {oneWord} & {anotherWord}")
