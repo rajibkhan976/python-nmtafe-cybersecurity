50d22c86723c58b534ab358fd5c5fd5afe008126
rajibkhan976 <rajibkhan976@gmail.com>
2025-09-21 10:10:17 +0800

    lab 8 practice

--- lab-8.py
+++ lab-8.py
@@ -7,60 +7,60 @@ favSongs = ("Ami", "Tumi", "Amra", "Tomra", "Tara")
 for i in favSongs:
     print(i)
 
-# aMovieList = []
-# aMovieList.append("Terminator 2")
-# aMovieList.append("Grave of the Fireflies")
-# aMovieList.append("Back to the Future")
-# aMovieList.append("Alien")
-# aMovieList.append("What we do in the Shadows")
-# aMovieList.append("Aqua Teen Hunger Force Colon Movie Film for Theatres")
-# aMovieList.append("Coraline")
+aMovieList = []
+aMovieList.append("Terminator 2")
+aMovieList.append("Grave of the Fireflies")
+aMovieList.append("Back to the Future")
+aMovieList.append("Alien")
+aMovieList.append("What we do in the Shadows")
+aMovieList.append("Aqua Teen Hunger Force Colon Movie Film for Theatres")
+aMovieList.append("Coraline")
 
-# # print(aMovieList)
+print(aMovieList)
 
-# counter = 1
-# while counter < 8:
-#     movie = input("Please enter a movie name: ")
-#     if movie.strip():
-#         aMovieList.append(movie)
-#         print(f"{movie} entered in the movie list.")
-#         counter += 1
+counter = 1
+while counter < 8:
+    movie = input("Please enter a movie name: ")
+    if movie.strip():
+        aMovieList.append(movie)
+        print(f"{movie} entered in the movie list.")
+        counter += 1
 
-# with open("movie-list.txt", "w") as f:
-#     for m in aMovieList:
-#         f.write(m + "\n")
+with open("movie-list.txt", "w") as f:
+    for m in aMovieList:
+        f.write(m + "\n")
 
-# rem_mov = input("Please enter the name of the movie you want to remove: ")
-# if aMovieList.count(rem_mov.strip()) > 0:
-#     aMovieList.remove(rem_mov)
-#     print(f"{rem_mov} removed from the list")
-# else:
-#     print(f"{rem_mov} does not exist in the movie list")
-# print(aMovieList)
+rem_mov = input("Please enter the name of the movie you want to remove: ")
+if aMovieList.count(rem_mov.strip()) > 0:
+    aMovieList.remove(rem_mov)
+    print(f"{rem_mov} removed from the list")
+else:
+    print(f"{rem_mov} does not exist in the movie list")
+print(aMovieList)
 
-# search_mov = input("Please enter the name of the movie you want to search: ")
-# if aMovieList.count(search_mov.strip()) > 0:
-#     print(f"{search_mov} movie exist in the list")
-# else:
-#     print(f"{search_mov} does not exist in the movie list")
+search_mov = input("Please enter the name of the movie you want to search: ")
+if aMovieList.count(search_mov.strip()) > 0:
+    print(f"{search_mov} movie exist in the list")
+else:
+    print(f"{search_mov} does not exist in the movie list")
 
-# print([23, 72, 12])
+print([23, 72, 12])
 
-# num = [1, 50, 23, 77, 12, 23, 57, 2, 7]
-# print(min(num))
+num = [1, 50, 23, 77, 12, 23, 57, 2, 7]
+print(min(num))
 
-# x = "a big string of many words to become a list"
-# y = x.split(" ")
-# print(y)
-# type(y)
-# z = " ".join(y)
-# print(z)
+x = "a big string of many words to become a list"
+y = x.split(" ")
+print(y)
+type(y)
+z = " ".join(y)
+print(z)
 
-# a_list = []
-# a_list.append("Red")
-# a_list.append("Green")
-# a_list.append("Orange")
-# a_list.append("Purple")
-# a_list.append("Cyan")
-# a_list.insert(1, "Yellow")
-# print(a_list)
+a_list = []
+a_list.append("Red")
+a_list.append("Green")
+a_list.append("Orange")
+a_list.append("Purple")
+a_list.append("Cyan")
+a_list.insert(1, "Yellow")
+print(a_list)
